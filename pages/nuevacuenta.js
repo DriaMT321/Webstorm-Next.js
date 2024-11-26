04a2d0167f52cc6512d0886cbdaa3948b10d0eca
DriaMT321 <dagneremt@gmail.com>
2024-11-26 11:59:01 -0400

    first commit

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import Layout from '../components/Layout';
+
+const NuevaCuenta = () => {
+
+    return ( 
+
+        <>
+            <Layout>
+
+                <h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
+
+            </Layout>
+        </>
+     );
+}
+ 
+export default NuevaCuenta;
